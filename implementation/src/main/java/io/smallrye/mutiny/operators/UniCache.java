6eb0881170b62909d34282429f279b149a86eebe
Julien Ponge <jponge@redhat.com>
2020-09-11 14:34:48 +0200

    DRAFT 2

--- implementation/src/main/java/io/smallrye/mutiny/operators/UniCache.java
+++ implementation/src/main/java/io/smallrye/mutiny/operators/UniCache.java
@@ -3,8 +3,6 @@ package io.smallrye.mutiny.operators;
 import static io.smallrye.mutiny.helpers.ParameterValidation.nonNull;
 
 import java.util.HashSet;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.subscription.UniSubscriber;
@@ -12,15 +10,17 @@ import io.smallrye.mutiny.subscription.UniSubscription;
 
 public class UniCache<T> extends UniOperator<T, T> implements UniSubscriber<T> {
 
-    private static final int INIT = 0;
-    private static final int SUBSCRIBING = 1;
-    private static final int SUBSCRIBED = 2;
-    private static final int RESOLVED = 3;
+    private enum State {
+        INIT,
+        SUBSCRIBING,
+        SUBSCRIBED,
+        RESOLVED
+    }
 
-    private final AtomicInteger state = new AtomicInteger(INIT);
-    private final AtomicReference<UniSubscription> upstreamSubscription = new AtomicReference<>();
     private final HashSet<UniSubscriber<? super T>> subscribers = new HashSet<>();
 
+    private volatile State state = State.INIT;
+    private volatile UniSubscription upstreamSubscription;
     private volatile T item;
     private volatile Throwable failure;
 
@@ -30,27 +30,73 @@ public class UniCache<T> extends UniOperator<T, T> implements UniSubscriber<T> {
 
     @Override
     protected void subscribing(UniSerializedSubscriber<? super T> subscriber) {
-        synchronized (state) {
+        synchronized (this) {
             subscribers.add(subscriber);
+            switch (state) {
+                case INIT:
+                    state = State.SUBSCRIBING;
+                    AbstractUni.subscribe(upstream(), this);
+                    break;
+                case SUBSCRIBING:
+                    break;
+                case SUBSCRIBED:
+                    passSubscription(subscriber);
+                    break;
+                case RESOLVED:
+                    passSubscriptionAndForward(subscriber);
+                    break;
+                default:
+                    throw new IllegalStateException("We are in state " + state);
+            }
         }
-        if (state.compareAndSet(INIT, SUBSCRIBING)) {
-            AbstractUni.subscribe(upstream(), this);
-        } else if (state.get() == SUBSCRIBED) {
-            passSubscription(subscriber);
-        } else if (state.get() == RESOLVED) {
-            passSubscriptionAndForward(subscriber);
-        } else {
-            throw new IllegalStateException("Unknown state " + state.get());
+    }
+
+    @Override
+    public void onSubscribe(UniSubscription subscription) {
+        synchronized (this) {
+            if (upstreamSubscription != null) {
+                throw new IllegalStateException("Invalid state " + state + " - received a second subscription from source");
+            }
+            if (state != State.SUBSCRIBING) {
+                throw new IllegalStateException("Invalid state " + state + " - should be SUBSCRIBING");
+            }
+            state = State.SUBSCRIBED;
+            this.upstreamSubscription = subscription;
+            dispatchSubscriptions();
+        }
+    }
+
+    @Override
+    public void onItem(T item) {
+        synchronized (this) {
+            if (state != State.SUBSCRIBED) {
+                throw new IllegalStateException(
+                        "Invalid state " + state + " - received item while we where not in the SUBSCRIBED state");
+            }
+            state = State.RESOLVED;
+            this.item = item;
+            forwardResultToSubscribers();
+        }
+    }
+
+    @Override
+    public void onFailure(Throwable failure) {
+        synchronized (this) {
+            if (state != State.SUBSCRIBED) {
+                throw new IllegalStateException(
+                        "Invalid state " + state + " - received failure while we where not in the SUBSCRIBED state");
+            }
+            state = State.RESOLVED;
+            this.failure = failure;
+            forwardResultToSubscribers();
         }
     }
 
     private void forwardResultToSubscribers() {
-        synchronized (state) {
-            for (UniSubscriber<? super T> subscriber : subscribers) {
-                forwardResult(subscriber);
-            }
-            subscribers.clear();
+        for (UniSubscriber<? super T> subscriber : subscribers) {
+            forwardResult(subscriber);
         }
+        subscribers.clear();
     }
 
     private void forwardResult(UniSubscriber<? super T> subscriber) {
@@ -64,62 +110,22 @@ public class UniCache<T> extends UniOperator<T, T> implements UniSubscriber<T> {
     }
 
     private void passSubscriptionAndForward(UniSerializedSubscriber<? super T> subscriber) {
-        synchronized (state) {
-            passSubscription(subscriber);
-            forwardResult(subscriber);
-            subscribers.remove(subscriber);
-        }
+        passSubscription(subscriber);
+        forwardResult(subscriber);
+        subscribers.remove(subscriber);
     }
 
     private void dispatchSubscriptions() {
-        synchronized (state) {
-            for (UniSubscriber<? super T> subscriber : subscribers) {
-                passSubscription(subscriber);
-            }
+        for (UniSubscriber<? super T> subscriber : subscribers) {
+            passSubscription(subscriber);
         }
     }
 
     private void passSubscription(UniSubscriber<? super T> subscriber) {
-        synchronized (state) {
-            subscriber.onSubscribe(() -> {
-                synchronized (state) {
-                    subscribers.remove(subscriber);
-                }
-            });
-        }
-    }
-
-    @Override
-    public void onSubscribe(UniSubscription subscription) {
-        if (upstreamSubscription.compareAndSet(null, subscription)) {
-            if (!state.compareAndSet(SUBSCRIBING, SUBSCRIBED)) {
-                throw new IllegalStateException("Invalid state " + state.get() + " - should be SUBSCRIBING");
+        subscriber.onSubscribe(() -> {
+            synchronized (UniCache.this) {
+                subscribers.remove(subscriber);
             }
-            dispatchSubscriptions();
-        } else {
-            throw new IllegalStateException("Invalid state " + state.get() + " - received a second subscription from source");
-        }
-    }
-
-    @Override
-    public void onItem(T item) {
-        if (state.get() != SUBSCRIBED) {
-            throw new IllegalStateException(
-                    "Invalid state " + state.get() + " - received item while we where not in the SUBSCRIBED state");
-        }
-        state.set(RESOLVED);
-        this.item = item;
-        forwardResultToSubscribers();
-    }
-
-    @Override
-    public void onFailure(Throwable failure) {
-        if (state.get() != SUBSCRIBED) {
-            throw new IllegalStateException(
-                    "Invalid state " + state.get() + " - received failure while we where not in the SUBSCRIBED state");
-        }
-        state.set(RESOLVED);
-        this.failure = failure;
-        forwardResultToSubscribers();
+        });
     }
 }
