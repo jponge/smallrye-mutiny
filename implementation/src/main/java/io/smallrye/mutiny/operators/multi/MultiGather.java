ed6cfdaf5f837f8002b2d1ac3e7f550ca070e753
Julien Ponge <jponge@redhat.com>
2025-03-31 22:22:23 +0200

    Gather smoke test

---
+++ implementation/src/main/java/io/smallrye/mutiny/operators/multi/MultiGather.java
@@ -0,0 +1,114 @@
+package io.smallrye.mutiny.operators.multi;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.helpers.Subscriptions;
+import io.smallrye.mutiny.subscription.MultiSubscriber;
+import io.smallrye.mutiny.tuples.Tuple2;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class MultiGather<I, ACC, O> extends AbstractMultiOperator<I, O> {
+
+    Supplier<ACC> initialAccumulatorSupplier;
+    BiFunction<ACC, I, ACC> accumulator;
+    Function<ACC, Optional<Tuple2<ACC, O>>> mapper;
+    Function<ACC, Optional<O>> finalizer;
+
+    public MultiGather(Multi<? extends I> upstream,
+                       Supplier<ACC> initialAccumulatorSupplier,
+                       BiFunction<ACC, I, ACC> accumulator,
+                       Function<ACC, Optional<Tuple2<ACC, O>>> mapper,
+                       Function<ACC, Optional<O>> finalizer) {
+        super(upstream);
+        this.initialAccumulatorSupplier = initialAccumulatorSupplier;
+        this.accumulator = accumulator;
+        this.mapper = mapper;
+        this.finalizer = finalizer;
+    }
+
+    @Override
+    public void subscribe(MultiSubscriber<? super O> subscriber) {
+        upstream.subscribe().withSubscriber(new MultiGatherProcessor(subscriber));
+    }
+
+    class MultiGatherProcessor extends MultiOperatorProcessor<I, O> {
+
+        private ACC acc;
+        private final AtomicLong demand = new AtomicLong();
+
+        public MultiGatherProcessor(MultiSubscriber<? super O> downstream) {
+            super(downstream);
+            this.acc = initialAccumulatorSupplier.get();
+            if (accumulator == null) {
+                throw new IllegalArgumentException("The initial accumulator cannot be null");
+            }
+        }
+
+        @Override
+        public void request(long numberOfItems) {
+            if (numberOfItems <= 0) {
+                onFailure(new IllegalArgumentException("The number of items requested must be strictly positive"));
+                return;
+            }
+            if (upstream != Subscriptions.CANCELLED) {
+                Subscriptions.add(demand, numberOfItems);
+                upstream.request(1L);
+            }
+        }
+
+        @Override
+        public void onItem(I item) {
+            if (upstream == Subscriptions.CANCELLED) {
+                return;
+            }
+            acc = accumulator.apply(acc, item);
+            if (acc == null) {
+                onFailure(new NullPointerException("The accumulator returned a null value"));
+                return;
+            }
+            Optional<Tuple2<ACC, O>> mapping = mapper.apply(acc);
+            if (mapping == null) {
+                onFailure(new NullPointerException("The mapper returned a null value"));
+                return;
+            }
+            if (mapping.isPresent()) {
+                Tuple2<ACC, O> tuple = mapping.get();
+                acc = tuple.getItem1();
+                O value = tuple.getItem2();
+                if (acc == null) {
+                    onFailure(new NullPointerException("The mapper returned a null accumulator value"));
+                    return;
+                }
+                if (value == null) {
+                    onFailure(new NullPointerException("The mapper returned a null value to emit"));
+                    return;
+                }
+                downstream.onItem(value);
+                long remaining = demand.decrementAndGet();
+                if (remaining > 0L) {
+                    upstream.request(1L);
+                }
+            } else {
+                upstream.request(1L);
+            }
+        }
+
+        @Override
+        public void onCompletion() {
+            if (upstream == Subscriptions.CANCELLED) {
+                return;
+            }
+            Optional<O> finalValue = finalizer.apply(acc);
+            if (finalValue == null) {
+                onFailure(new NullPointerException("The finalizer returned a null value"));
+                return;
+            }
+            finalValue.ifPresent(o -> downstream.onItem(o));
+            downstream.onCompletion();
+        }
+    }
+}
---
+++ implementation/src/test/java/io/smallrye/mutiny/operators/multi/MultiGatherTest.java
@@ -0,0 +1,47 @@
+package io.smallrye.mutiny.operators.multi;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.helpers.test.AssertSubscriber;
+import io.smallrye.mutiny.tuples.Tuple2;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiGatherTest {
+
+    @Test
+    void smoke_test() {
+        AssertSubscriber<ArrayList<Integer>> sub = Multi.createFrom().range(1, 100)
+                .onItem().gather(
+                        () -> new ArrayList<Integer>(),
+                        (list, next) -> {
+                            list.add(next);
+                            return list;
+                        },
+                        list -> {
+                            if (list.size() > 5) {
+                                return Optional.of(Tuple2.of(new ArrayList<>(), list));
+                            } else {
+                                return Optional.empty();
+                            }
+                        },
+                        list -> {
+                            if (list.isEmpty()) {
+                                return Optional.empty();
+                            } else {
+                                return Optional.of(list);
+                            }
+                        }
+                )
+                .subscribe().withSubscriber(AssertSubscriber.create());
+
+        sub.awaitNextItems(2);
+        System.out.println(sub.getItems());
+        sub.request(Long.MAX_VALUE);
+        sub.awaitCompletion();
+        System.out.println(sub.getItems());
+    }
+}
